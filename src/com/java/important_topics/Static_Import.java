423e8e0da60cf4ba88628027c1f59e39d05b8345
Rupa <roopa.rupa99@gmail.com>
2019-11-17 11:11:53 +0530

    Java commit 5

---
+++ src/com/java/important_topics/Static_Import.java
@@ -0,0 +1,19 @@
+package com.java.important_topics;
+
+//import com.java.basics.Method_1;  //this is normal import
+
+import static com.java.basics.Method_1.m2;	//this is static import
+
+public class Static_Import {
+
+	public static void main(String[] args) {
+		
+		
+		//Method_1.m2();  //calling with normal import
+		
+		m2();			 //	calling using static import
+
+		
+	}
+
+}
